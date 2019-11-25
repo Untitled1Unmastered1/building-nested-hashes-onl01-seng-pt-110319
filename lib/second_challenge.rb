1f4eec9593488da37575178b0c4ca3b057242f46
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-25 23:13:50 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,15 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive,
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-     }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
-end
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
